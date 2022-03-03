b44fc6f6373c3b8ca2bd34b410cc5c0464f04678
HrikB <hrik.bhowal@gmail.com>
2022-03-02 22:21:20 -0800

    User interface

---
+++ types/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  profilePic: string;
+  friendsList: string[];
+}
