2f6b8fa0e016b30afdd2c42f6821255dc21ede70
HrikB <hrik.bhowal@gmail.com>
2022-03-08 11:32:16 -0800

    new types for socket io acknowledgements

--- types/socket.io/ClientToServerEvents.ts
+++ types/socket.io/ClientToServerEvents.ts
@@ -1,8 +1,15 @@
 import { UserEvents } from "../events";
 import { User } from "..";
 
+interface Response {
+  error: string;
+  errorDetails: string;
+}
+
+type Acknowledgment = (res: Response) => void;
+
 type ClientToServerEvents = {
-  [key in UserEvents]: (userChanges: User) => void;
+  [key in UserEvents]: (userChanges: User, cb: Acknowledgment) => void;
 };
 
 export default ClientToServerEvents;
