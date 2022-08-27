300622aac157c24132e7797e73b678f9922f8281
HrikB <hrik.bhowal@gmail.com>
2022-08-26 23:41:52 -0700

    use esbuild-loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,79 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
 const nextConfig = {
-  reactStrictMode: true,
+  webpack(config, { isServer, dev }) {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        React: 'react'
+      })
+    );
+
+    useEsbuildMinify(config, {
+      css: true
+    });
+
+    useEsbuildLoader(config, {
+      // Specify `tsx` if you're using TypeSCript
+      loader: 'tsx',
+      target: 'es2017'
+    });
+    return config;
+  }
+};
+
+function useEsbuildMinify(config, options) {
+  const terserIndex = config.optimization.minimizer.findIndex(
+    minimizer => minimizer.constructor.name === 'TerserPlugin'
+  );
+  if (terserIndex > -1) {
+    config.optimization.minimizer.splice(
+      terserIndex,
+      1,
+      new ESBuildMinifyPlugin(options)
+    );
+  }
 }
 
-module.exports = nextConfig
+function useEsbuildLoader(config, options) {
+  const esbuildLoader = { loader: 'esbuild-loader', options };
+  config.module.rules = config.module.rules.map(rule => {
+    if (rule.test && rule.test.test('.js')) {
+      // use
+      if (rule.use) {
+        // Array
+        if (Array.isArray(rule.use)) {
+          rule.use = rule.use.map(loader => {
+            if (
+              typeOf(loader) === 'Object' &&
+              loader.loader === 'next-babel-loader'
+            ) {
+              return esbuildLoader;
+            }
+            return loader;
+          });
+        } else if (
+          typeOf(rule.use) === 'Object' &&
+          rule.use.loader &&
+          rule.use.loader === 'next-babel-loader'
+        ) {
+          // Object
+          rule.use = esbuildLoader;
+        }
+      }
+      // loader
+      if (rule.loader && rule.loader.loader === 'next-babel-loader') {
+        rule.loader = esbuildLoader;
+      }
+    }
+    return rule;
+  });
+}
+
+function typeOf(o) {
+  return Object.prototype.toString.call(o).slice(8, -1);
+}
+
+module.exports = nextConfig;
+
